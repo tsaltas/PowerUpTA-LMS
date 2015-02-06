ff1fd65434ae5be68351c6b3e1b59a98ca04e33d
Shannon Tsaltas <shannontsaltas@gmail.com>
2015-02-05 20:23:31 -0600

    created api routes

--- lms/lessons/urls.py
+++ lms/lessons/urls.py
@@ -1,6 +1,14 @@
-from django.conf.urls import patterns, url
+from django.conf.urls import patterns, url, include
 from lessons import views
 from rest_framework.routers import DefaultRouter
+
+# Create a router and register viewsets
+router = DefaultRouter()
+router.register(r'tags', views.TagViewSet)
+router.register(r'materials', views.MaterialViewSet)
+router.register(r'resources', views.ResourceViewSet)
+router.register(r'activities', views.ActivityViewSet)
+router.register(r'curricula', views.CurriculumViewSet)
 
 # API URLs determined automatically by the rest framework router
 # Include URLs for the browsable API
