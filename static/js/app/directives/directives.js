c936b3029f521fbeac23a8f37aff0153f49efcb9
Shannon Tsaltas <shannontsaltas@gmail.com>
2015-04-16 19:01:16 -0500

    add autofocus to new object forms

--- static/js/app/directives/directives.js
+++ static/js/app/directives/directives.js
@@ -24,3 +24,22 @@ lessonsDirectives.directive('myFileModel', ['$parse', function ($parse) {
         }
     };
 }]);
+
+/**
+ * the HTML5 autofocus property can be finicky when it comes to dynamically loaded
+ * templates and such with AngularJS. Use this simple directive to
+ * tame this beast once and for all.
+ *
+ * Usage:
+ * <input type="text" autofocus>
+ */
+lessonsDirectives.directive('autofocus', ['$timeout', function($timeout) {
+  return {
+    restrict: 'A',
+    link : function($scope, $element) {
+      $timeout(function() {
+        $element[0].focus();
+      });
+    }
+  }
+}]);
